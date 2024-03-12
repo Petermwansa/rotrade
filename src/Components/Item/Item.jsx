ede8fa6f6204a722ccacc0c7db151cd99d3c48b4
Petermwansa <mwansapeter800@gmail.com>
2024-03-12 23:22:17 +0300

    Add the popular component(section)

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -8,10 +8,10 @@ const Item = (props) => {
       <p>{props.name}</p>
       <div className='item__prices'>
         <div className='item__price__new'>
-            {props.new_price}
+            ${props.new_price}
         </div>
         <div className='item__price__old'>
-            {props.old_price}
+            ${props.old_price}
         </div>
       </div>
     </div>
