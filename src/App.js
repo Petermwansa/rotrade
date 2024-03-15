a324c9dd5328490434933ca3b5bb8156334d26ad
Petermwansa <mwansapeter800@gmail.com>
2024-03-16 00:33:50 +0300

    Finish the categories pages using the context api to share the data throughout the app

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Cart from './Components/Pages/Cart';
 import LoginSignUp from './Components/Pages/LoginSignUp';
 import Shop from './Components/Pages/Shop';
 import Footer from './Components/Footer/Footer';
+import men_banner from "../src/Components/Assests/banner_mens.png"
+import women_banner from "../src/Components/Assests/banner_women.png"
+import kids from "../src/Components/Assests/banner_kids.png"
 
 function App() {
   return (
@@ -16,9 +19,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Shop />} />
-          <Route path='/men' element={<ShopCategory category="men"/>} />
-          <Route path='/women' element={<ShopCategory category="women" />} />
-          <Route path='/kids' element={<ShopCategory  category="kids" />} />
+          <Route path='/men' element={<ShopCategory banner={men_banner} category="men"/>} />
+          <Route path='/women' element={<ShopCategory banner={women_banner} category="women" />} />
+          <Route path='/kids' element={<ShopCategory banner={kids}  category="kid" />} />
           <Route path='/products' element={<Products />} >
             <Route path=':productId' element={<Products />} />
           </Route>
--- src/Components/Pages/ShopCategory.jsx
+++ src/Components/Pages/ShopCategory.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import './CSS/ShopCategory.css';
+import { ShopContext } from '../../Context/Context';
+import dropdown_icon from '../Assests/dropdown_icon.png'
+import Item from '../Item/Item';
 
-const ShopCategory = () => {
+const ShopCategory = (props) => {
+    const {all_product} = useContext(ShopContext)
+
+
   return (
-    <div>
-      
+    <div className='shop__category'>
+      <img className='shopCategory__banner' src={props.banner} alt='' />
+      <div className='shopcategory__indexSort'>
+        <p>
+            <span>Showing 1-12</span> out of <span>36</span> products
+        </p>
+        <div className='shopcategory__sort'>
+            Sort by <img src={dropdown_icon} alt='' />
+        </div>
+      </div>
+      <div className='shopcategory__products'>
+        {all_product.map((item, index) => {
+            if(props.category === item.category) {
+                return <Item key={index} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+            } else {
+                return null;
+            }
+        })}
+      </div>
+      <div className='shopcategory__loadmore'>
+        Explore More
+      </div>
     </div>
   )
 }
