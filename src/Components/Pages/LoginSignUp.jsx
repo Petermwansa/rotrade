06fe7527207471565aeff1c2fe89978a7e0c8c33
Petermwansa <mwansapeter800@gmail.com>
2024-03-17 23:28:09 +0300

    Add the code for the Login page but has to be designed later

--- src/Components/Pages/LoginSignUp.jsx
+++ src/Components/Pages/LoginSignUp.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import './CSS/LoginSignup.css';
 
 const LoginSignUp = () => {
   return (
-    <div>
-      
+    <div className='loginsignup'>
+      <div className='loginsignup__container'>
+        <h1>Sign Up</h1>
+        <div className='loginsignup__fields'>
+          <input type='text' placeholder='Your name' />
+          <input type='email' placeholder='Email address' />
+          <input type='passoword' placeholder='Password' />
+        </div>
+        <button>Continue</button>
+        <p className='loginsignup__login'>Already have an acount? <span>Login here</span></p>
+        <div className='loginsignup__agree'>
+          <input type='checkbox' name='' id=''/>
+          <p>I agree to the terms and privacy polichy</p>
+        </div>
+      </div>
     </div>
   )
 }
