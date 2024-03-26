ffd2bc7ba326312b8586ae75c85752e2f1e975ec
Petermwansa <mwansapeter800@gmail.com>
2024-03-26 23:52:57 +0300

    Add the designs for the cart

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -23,18 +23,24 @@ const CartItems = () => {
         {all_product.map((e) => {
             if(cartItems[e.id] > 0) {
                 return  <div>
-                            <div className='cartitems__format'>
+                            <div className='cartitems__format caertitems__format__main'>
                                 <img src={e.image} className='cartitem__product__icon' alt=''/>
                                 <p>{e.name}</p>
                                 <p>${e.new_price}</p>
                                 <button className='cartitems__quantity'>{cartItems[e.id]}</button>
-                                <p>{e.new_price * cartItems[e.id]}</p>
-                                <img onClick={() => {removeFromCart(e.id)}} src={remove__icon} alt='' className='' />
+                                <p>${e.new_price * cartItems[e.id]}</p>
+                                <img className="cartitems__remove__icon" onClick={() => {removeFromCart(e.id)}} src={remove__icon} alt=''/>
                             </div>
                             <hr />
                         </div>
             }
+            return null;
         })}
+        <div className='cartitems__down'>
+          <div className='cartitems__total'>
+            <h1>Cart Total</h1>
+          </div>
+        </div>
       </div>
     </div>
   )
